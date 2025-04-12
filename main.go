3bef25202bf8b239eee8f80c28f7d8951d5204c6
eridon-pro <take@erikawa.com>
2025-04-12 22:26:33 +0900

    pre sync hook

--- main.go
+++ main.go
@@ -48,7 +48,7 @@ func Goodbye() echo.HandlerFunc {
 
 func Thankyou() echo.HandlerFunc {
     return func(c echo.Context) error {     
-        return c.String(http.StatusOK, "thank you!!")
+        return c.String(http.StatusOK, "thank you!")
     }
 }
 
